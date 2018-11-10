318fe8939f35e3c2638bfd0f30442a307fa1f670
yuhyeon <dbgus0101@gmail.com>
2018-11-10 20:40:00 +0900

    null check

--- src/component/input.js
+++ src/component/input.js
@@ -8,19 +8,30 @@ export default class input extends Component {
     id: 1
   }
   inputChange = (input) => {
-    this.setState({
-      todo: input.target.value
-    })
+    if (input.target.value !== null) {
+      this.setState({
+        todo: input.target.value
+      })
+    }
+    else {
+      alert("값을 넣어")
+    }
   }
   submitButton = (parm) => {
-    const id = parm.target.name
-    const content = this.state.todo
-    localStorage.setItem(id, content)
-    this.setState({
-      todo: '',
-      id: this.state.id + 1
-    })
-    this.props.onCreate(this.state.id);
+    const id = parm.target.name;
+
+    const content = this.state.todo;
+    if (content !== '') {
+      localStorage.setItem(id, content)
+      this.setState({
+        todo: '',
+        id: this.state.id + 1
+      })
+      this.props.onCreate(this.state.id);
+    }
+    else {
+      console.log('값 null')
+    }
   }
 
   render() {
